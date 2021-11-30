45ee676c38e704cb4b1c53569a9557a0c015e19b
Riccardo-Grasso <riccardo.grasso@outlook.it>
2021-11-30 14:58:27 +0100

    header

---
+++ index.php
@@ -0,0 +1,68 @@
+<?php
+$navLinks
+?>
+
+
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="styles/main.css">
+    <title>Google</title>
+</head>
+
+<body>
+    <header>
+        <div class="container-fluid">
+            <div class="row-1 d-flex align-items-center justify-content-between">
+                <div class="logo d-flex align-items-center">
+                    <a href="https://www.google.it/" class="h-100">
+                        <img src="img/google_logo.png" alt="logo_google" class="h-100">
+                    </a>
+
+                    <span class="fs-22 ">
+                        <a href="#" class="privacy text-grey">Privacy e termini</a>
+                    </span>
+                </div>
+                <div class="user d-flex align-items-center h-50">
+                    <a href="#">
+                        <i class="fas fa-th text-grey"></i>
+                    </a>
+                    <div class="h-100 ms-4">
+                        <img src="img/user-icon-image-placeholder.jpg" class="rounded-circle h-100" alt="user">
+                    </div>
+                </div>
+            </div>
+            <div class="row-2 d-flex">
+                <nav class="my-navbar">
+                    <ul class="pages-list d-flex text-grey fs-15">
+                        <li class="page ps-2">
+                            <a href="#">Introduzione</a>
+                        </li>
+                        <li class="page">
+                            <a href="#">Norme sulla privacy</a>
+                        </li>
+                        <li class="page">
+                            <a href="#">Termini di servizio</a>
+                        </li>
+                        <li class="page">
+                            <a href="#">Tecnologie</a>
+                        </li>
+                        <li class="page">
+                            <a href="#">Domande frequenti</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+
+        </div>
+        <hr>
+    </header>
+</body>
+
+</html>
